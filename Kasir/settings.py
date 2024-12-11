11b7fc33e40dab37ff174e07d498b4a12d241cd9
ZakariaSAM <yahyamuhammad350@gmail.com.com>
2024-12-11 10:08:58 +0700

    reset database

--- Kasir/settings.py
+++ Kasir/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-n-q9ewppopxr@b0b$6x*+_g%io)@q*&g5e93u)*-&5xvdgn)p_
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app']
+ALLOWED_HOSTS = ['.vercel.app', "127.0.0.1"]
 
 
 # Application definition
